887b9b42bb6092a2728c25ac416d0a39a978e811
tylergeery <tyler.geery@yahoo.com>
2016-10-16 22:05:24 -0700

    Pull user structs, start MVC architecture

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # pick-up-api
 Pick Up App
 
-
 ## Running Pickup Locally
 ```
 # Run from the root of this directory
 docker build --rm -t pickup-pg config/db
 docker build --rm -t pickup-go-server -f config/api/Dockerfile .
 docker run -p 5432:5432 --name pickup-db -e POSTGRES_PASSWORD=pickEmUp -d pickup-pg
-docker run -p 3000:3000 -v $(pwd)/api:/go/src/pickup-server --name pickup-server --link pickup-db:pickup-postgres -d pickup-go-server
+docker run -p 3000:3000 -v $(pwd)/api:/go/src/github.com/pick-up-api --name pickup-server --link pickup-db:pickup-postgres -d pickup-go-server
 ```
+
+### Helpful articles
+[Golang Docker](https://blog.golang.org/docker)
+[Dockerizing Go w/ Local Filesync](https://medium.com/developers-writing/docker-powered-development-environment-for-your-go-app-6185d043ea35#.r58sq9cr2)
+[Architecting Go Web Apps](https://larry-price.com/blog/2015/06/25/architecture-for-a-golang-web-app)
--- api/main.go
+++ api/main.go
@@ -4,19 +4,15 @@ import (
     "fmt"
     "log"
     "net/http"
-
     "github.com/gorilla/mux"
-
-    "database/sql"
-
-	_ "github.com/lib/pq"
+    "github.com/pick-up-api/controllers"
 )
 
 func main() {
 
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/hello/{name}", Hello)
-    router.HandleFunc("/user/{userId}", UserProfile)
+    router.HandleFunc("/user/{userId}", controllers.UserProfile)
 
     log.Fatal(http.ListenAndServe(":3001", router))
 }
@@ -24,19 +20,4 @@ func main() {
 func Hello(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     fmt.Fprintf(w, "Hello, %s!", vars["name"])
-}
-
-func UserProfile(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    userId := vars["userId"]
-    fmt.Fprintln(w, "Finding user with id:", userId)
-
-    db, err := sql.Open("postgres", "postgres://raccoon:password@192.168.99.100/pqgotest?sslmode=disable")
-	if err != nil {
-        fmt.Fprintln(w, "Found error: ", err)
-		log.Fatal(err)
-	}
-
-	rows, err := db.Query("SELECT * FROM users WHERE id = $id", userId)
-    fmt.Fprintln(w, "Found rows: ", rows)
 }
