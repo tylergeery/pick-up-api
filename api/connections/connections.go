ac062deddf03a64f01961ad1a5702ca7b1acb284
tylergeery <tyler.geery@yahoo.com>
2016-10-21 11:57:04 -0700

    improve connections

---
+++ api/connections/connections.go
@@ -0,0 +1,22 @@
+package connections
+
+import (
+    "log"
+    "database/sql"
+	_ "github.com/lib/pq"
+)
+
+/**
+ * Get a SQL DB connection
+ */
+func DB() *sql.DB {
+    db, err := sql.Open(
+        "postgres",
+        "postgres://raccoon:pickEmUp@pickup-postgres/pickup?sslmode=disable")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return db
+}
