982366c397df72163c3742248d706dc24cf48ce6
tylergeery <tyler.geery@yahoo.com>
2017-03-28 08:38:05 -0700

    rename user seed file

---
+++ config/db/seed/user_token.sql
@@ -0,0 +1,10 @@
+CREATE TABLE user_tokens (
+    user_id PRIMARY KEY,
+    refresh_token VARCHAR(250),
+    access_token VARCHAR(250),
+    created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'PST'),
+    updated_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'PST')
+);
+
+INSERT INTO user_tokens (user_id, refresh_token, created_at)
+VALUES (1,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9ucyI6MSwidG9rZW5UeXBlIjoicmVmcmVzaCIsInRzIjoxNDkwNTkyMDcyLCJ0eXBlIjoidXNlciIsInVzZXJJZCI6MH0.6fnQTQ9iqdp-3duuL4A_WQ6tiyHVUvmY3bKYx1VFIms', NOW());
